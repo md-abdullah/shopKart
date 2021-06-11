72486c91bdb06b757871ea2758ccaa39c64cec62
md-abdullah <mohabdul.mgr@gmail.com>
2021-06-11 11:02:59 +0530

    first-commit

---
+++ books/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here.
+
+
+class Author(models.Model):
+    title = models.CharField(max_length=200)
+    now = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    pageCount = models.IntegerField()
+    thumbnailUrl = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return f"{self.id}-{self.name}"
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=200)
+    book = models.ForeignKey(Book, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
